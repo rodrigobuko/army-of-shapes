3e6c3234fc70ba6bc84b7c4e7a3561489b26c145
Rodrigo Malhães Bukowitz <39204408+rodrigomalhaesbuko@users.noreply.github.com>
2022-11-27 14:04:43 -0300

    Game Events and New Buffs

---
+++ Army of shapes/Assets/Scripts/ScriptableObjects/Game/GameType.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TypeOfGame
+{
+    RandomBattle,
+    BetBattle
+}
+[CreateAssetMenu(menuName = "Army of Shapes/GameType")]
+public class GameType : ScriptableObject
+{
+    private TypeOfGame _typeOfGame;
+
+    public TypeOfGame TypeOfGame => _typeOfGame;
+    
+    public void SetGameType(TypeOfGame gameType)
+    {
+        _typeOfGame = gameType;
+    }
+}
---
+++ Army of shapes/Assets/Scripts/ScriptableObjects/Game/PlayerScore.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Army of Shapes/PlayerScore")]
+public class PlayerScore : ScriptableObject
+{
+    public string ScoreKey;
+    public int Score;
+
+    public int GetHighScore()
+    {
+        if (PlayerPrefs.HasKey(ScoreKey))
+        {
+            return PlayerPrefs.GetInt(ScoreKey);
+        }
+
+        return 0;
+    }
+
+    public void SaveHighScore()
+    {
+        if (!PlayerPrefs.HasKey(ScoreKey))
+        {
+            PlayerPrefs.SetInt(ScoreKey, 0);
+        }
+
+        int highscore = PlayerPrefs.GetInt(ScoreKey);
+        if (Score > highscore)
+        {
+            PlayerPrefs.SetInt(ScoreKey, Score);
+        }
+    }
+}
